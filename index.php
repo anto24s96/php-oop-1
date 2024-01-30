a86946c0c44e1363bf473a355220149627a968b4
anto24s96 <anto24s96@gmail.com>
2024-01-30 16:44:50 +0100

    [BONUS 1/2] exercise completed

--- index.php
+++ index.php
@@ -30,16 +30,16 @@
     }
 
     /* CREAZIONE DELLE ISTANZE MOVIE */
-    $movie_1 = new Movie('2012', 'Sci-Fi', 2009, 'USA, Canada');
+    $movie_1 = new Movie('2012', ['Action', 'Drama', 'Sci-Fi'], 2009, 'USA, Canada');
     $movie_1->setHours(158);
 
-    $movie_2 = new Movie('The Day After Tomorrow', 'Sci-Fi', 2004, 'USA' );
+    $movie_2 = new Movie('The Day After Tomorrow', ['Drama', 'Sci-Fi', 'Thriller'], 2004, 'USA' );
     $movie_2->setHours(125);
 
-    $movie_3 = new Movie('Giustizia Privata', 'Thriller', 2010, 'USA');
+    $movie_3 = new Movie('Giustizia Privata', ['Drama', 'Thriller', 'Crime'], 2010, 'USA');
     $movie_3->setHours(109);
 
-    $movie_4 = new Movie('La guerra dei mondi', 'Action, Sci-Fi', 2005, 'USA');
+    $movie_4 = new Movie('La guerra dei mondi', ['Action', 'Sci-Fi'], 2005, 'USA');
     $movie_4->setHours(103);
 
     $arrMovies = [
@@ -49,12 +49,37 @@
         $movie_4
     ];
 
-    var_dump($arrMovies);
-
-    foreach ($arrMovies as $movie) {
-        echo "<strong>$movie->name</strong>"  .' è uscito nel '.$movie->year.' è un film di genere '.$movie->genre.' prodotto in '.$movie->country. ' e dura '.$movie->getHours();
-        echo '<br>';
-        echo '<br>';
-    }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+    <div class="container text-white mt-5">
+        <div class="row">
+            <h1 class="fw-bolder">My Movies</h1>
+        </div>
+        <div class="row">
+            <div class="col-12 py-3">
+                <?php foreach ($arrMovies as $movie) { ?>                
+                    <div>Nome: <span class="fw-bolder"><?php echo $movie->name ?></span></div>
+                
+                    <div>Genere:
+                        <?php foreach ($movie->genre as $genre) { ?>
+                            <span><?php echo $genre ?></span>
+                        <?php } ?>
+                    </div>
+                    <div>Anno: <?php echo $movie->year ?></div> 
+                    <div class="mb-5">Durata: <?php echo $movie->duration ?></div>   
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
